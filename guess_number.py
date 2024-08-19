c29dd8c6842269d0c7d32cc6f2e836b58ef76fbb
Anastasia Koval <mrsanastasia.koval@gmail.com>
2024-08-19 16:55:47 +0300

    Some Change

--- guess_number.py
+++ guess_number.py
@@ -1,19 +1,18 @@
 from random import randint
 
-number = randint(1,100)
+number = randint(1, 100)
 print('Угадайте число от 1 до 100')
 
-while True: 
+while True:
     guess = int(input(('Введите число: ')))
 
-    if guess < number: 
+    if guess < number:
         print('Ваше число меньше того, что загадано.')
-    
-    elif guess > number: 
+
+    elif guess > number:
         print('Ваше число больше того, что загадано.')
 
-    elif guess == number: 
+    elif guess == number:
         break
 
 print('Отличная интуиция! Вы угадали число :)')
- 
